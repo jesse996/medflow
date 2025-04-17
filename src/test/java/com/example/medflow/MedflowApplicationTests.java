94b358fd701812e157d11c30666ff38306f1b9a4
jesse <szxfml@gmail.com>
2025-04-17 21:16:19 +0800

    init

---
+++ src/test/java/com/example/medflow/MedflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
